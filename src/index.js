5aa15340387d7f31d3ae9997566c48877393ca65
simonMoon83 <munduki@gmail.com>
2022-09-04 20:54:42 +0900

    Add hompage

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from 'components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode basename={process.env.PUBLIC_URL}>
     <App />
   </React.StrictMode>
 );
