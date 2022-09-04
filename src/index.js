6557a60992e47486af59ffd232c5bd192e306bc0
simonMoon83 <munduki@gmail.com>
2022-09-04 21:32:23 +0900

    build

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode basename={process.env.PUBLIC_URL}>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
