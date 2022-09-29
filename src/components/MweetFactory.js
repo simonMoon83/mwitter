b2b46f296d2c880d984b807ed45cc30e3c425c04
simonMoon83 <munduki@gmail.com>
2022-09-30 06:13:59 +0900

    Style 2

---
+++ src/components/MweetFactory.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "fbase";
+import { ref, uploadString, getDownloadURL } from "@firebase/storage";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+const MweetFactory = ({ userObj }) => {
+  const [mweet, setMweet] = useState("");
+  const [attachment, setAttachment] = useState("");
+  const onSubmit = async (event) => {
+    if (mweet === "") {
+        return;
+    }
+    event.preventDefault();
+    let attachmentUrl = "";
+
+    if (attachment !== "" && attachment !== null) {
+      const attachmentRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+      const response = await uploadString(attachmentRef, attachment, 'data_url');
+      attachmentUrl = await getDownloadURL(response.ref);
+    }
+    const mweetObj = {
+      text: mweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+      attachmentUrl,
+    };
+    await dbService.collection("mweets").add(mweetObj);
+    setMweet("");
+    setAttachment("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setMweet(value);
+  };
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    if (theFile !== undefined) {
+      reader.readAsDataURL(theFile);
+    }
+  };
+  const onClearAttachment = () => setAttachment("");
+  return (
+    <form onSubmit={onSubmit} className="factoryForm">
+      <div className="factoryInput__container">
+        <input
+          className="factoryInput__input"
+          value={mweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="&rarr;" className="factoryInput__arrow" />
+      </div>
+      <label htmlFor="attach-file" className="factoryInput__label">
+      <span>Add photos</span>
+        <FontAwesomeIcon icon={faPlus} />
+      </label>
+      <input
+        id="attach-file"
+        type="file"
+        accept="image/*"
+        onChange={onFileChange}
+        style={{
+        opacity: 0,
+        }}
+      />        
+      {attachment && (
+        <div className="factoryForm__attachment">
+            <img
+            src={attachment}
+            style={{
+                backgroundImage: attachment,
+            }}
+            />
+            <div className="factoryForm__clear" onClick={onClearAttachment}>
+            <span>Remove</span>
+            <FontAwesomeIcon icon={faTimes} />
+            </div>
+        </div>
+      )}
+    </form>
+  );
+};
+export default MweetFactory;
